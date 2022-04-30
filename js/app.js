4bd7b0f96aa8ab3d89bfe123bc7dd800383764d0
arjun <arjun259194@gmail.com>
2022-04-30 22:45:18 +0530

    Searching logic ready

--- js/app.js
+++ js/app.js
@@ -1,11 +1,15 @@
 //*VARIABLES
+const searchKeyword = document.querySelector('#keyWordInput');
+const countryName = document.querySelector('#country');
 const newsFeed = document.querySelector('.news-feed');
 const apiKey = 'a481486cec3745ddae53821bb5132799'
 const url = 'https://newsapi.org/v2/top-headlines?'
 let html = ``
 
+
 //*FUNCTIONS
 function appendDataToNewsFeed(data) {
+  html = ''
   for (const element of data) {
     html += `<article>
               <img src="${element.urlToImage}" alt="" />
@@ -34,11 +38,40 @@ async function defaultNewsLoad() {
       appendDataToNewsFeed(articles)
 
     })
-    .catch(err => console.log(err))
+    .catch(err => alert(err))
 }
 
-async function keywordNewsSearch() { }
-async function countryNewsSearch() { }
+
+async function keywordNewsSearch() {
+  let url = 'https://newsapi.org/v2/everything?'
+  let keyword = searchKeyword.value;
+  let sortBy = 'popularity'
+
+  await fetch(`${url}q=${keyword}&sortBy=${sortBy}&apiKey=${apiKey}`)
+    .then(response => response.json())
+    .then(json => {
+      let articles = json['articles']
+      appendDataToNewsFeed(articles)
+    })
+    .catch(err => alert(err))
+}
+
+
+async function countryNewsSearch() {
+  // 'https://newsapi.org/v2/top-headlines?country=us&apiKey=a481486cec3745ddae53821bb513279
+  let country = countryName.value
+
+  await fetch(`${url}country=${country}&apiKey=${apiKey}`)
+    .then(response => response.json())
+    .then(parsedResponse => {
+
+      let articles = parsedResponse['articles']
+
+      appendDataToNewsFeed(articles)
+
+    })
+    .catch(err => alert(err))
+}
 
 //*CALLING FUNCTIONS
 
