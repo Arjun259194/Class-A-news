0b6fc723095c4567846a0da35c2d4641019c159f
Arjun259194 <99578084+Arjun259194@users.noreply.github.com>
2022-05-06 20:49:19 +0530

    Revert "mahir joshi"

--- Class-A-news-main/js/themeToggle.js
+++
@@ -1,9 +0,0 @@
-//*VARIABLES
-let themeToggle = document.querySelector('.theme-toggle');
-
-
-//*Theme toggle
-themeToggle.addEventListener('click',e=>{
-  document.body.classList.toggle('dark-theme')
-  console.log('done');
-})
